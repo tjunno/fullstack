867fe69ddeaffec67f3c92030c61d3e1fc9ea423
Tuomas Junno <tuomas.junno@cs.helsinki.fi>
2018-01-30 19:16:33 +0200

    osa1

--- osa1/2/src/index.js
+++ osa1/2/src/index.js
@@ -2,40 +2,87 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+const Button = ({klik, nimi}) =>{
+  return (
+  <button onClick={klik}> {nimi} </button>
+  )
+}
+
+const Statistics = ({hyva, neutraali, huono, keskiarvo, kaikki}) =>{
+  const positiivisia = hyva / kaikki * 100 + '%'
+  if (kaikki===0)
+  return (<div><p>ei yhtään palautetta annettu</p></div>)
+  else
+  return (
+    
+    <table>
+      <tbody>
+    
+    <Statistic nimi='hyvä' arvo={hyva}/>
+    <Statistic nimi='neutraali' arvo={neutraali}/>
+    <Statistic nimi='huono' arvo={huono}/>
+    <Statistic nimi='keskiarvo' arvo={keskiarvo/kaikki}/>
+    <Statistic nimi='positiivisia' arvo={positiivisia}/>
+    </tbody>
+    </table>
+  )
+
+}
+
+const Statistic = ({nimi, arvo}) =>{
+  return (
+    <tr><td>{nimi}</td><td>{arvo}</td></tr>
+  )
+}
 class App extends React.Component {
-    constructor(props) {
-      super(props)
+    constructor() {
+      super()
       this.state = {
-        vasen: 0,
-        oikea: 0,
-        kaikki: []
+        hyva: 0,
+        neutraali: 0,
+        huono: 0,
+        kaikki: 0,
+        keskiarvo: 0
       }
     }
-  
-    klikVasen = () => {
-      this.setState({
-        vasen: this.state.vasen + 1,
-        kaikki: this.state.kaikki.concat('v')
-      })
+
+    klikkaa = (arvo) => {
+      if (arvo==='hyva')
+      return () => {
+        this.setState({
+          hyva: this.state.hyva + 1,
+          keskiarvo: this.state.keskiarvo + 1,
+          kaikki: this.state.kaikki + 1
+        })
+      }
+      if (arvo==='neutraali')
+      return () => {
+        this.setState({
+          neutraali: this.state.neutraali + 1,
+          keskiarvo: this.state.keskiarvo + 0,
+          kaikki: this.state.kaikki + 1
+        })
+      }
+      if (arvo==='huono')
+      return () => {
+        this.setState({
+          huono: this.state.huono + 1,
+          keskiarvo: this.state.keskiarvo - 1,
+          kaikki: this.state.kaikki + 1
+        }) 
+      }
+      
     }
-  
-    klikOikea = () => {
-      this.setState({
-        oikea: this.state.oikea + 1,
-        kaikki: this.state.kaikki.concat('o')
-      })
-    }
-  
     render() {
-        const historia = () => this.state.kaikki.join(' ')
         return (
           <div>
             <div>
-              {this.state.vasen}
-              <button onClick={this.klikVasen}>vasen</button>
-              <button onClick={this.klikOikea}>oikea</button>
-              {this.state.oikea}
-              <div>{historia()}</div>
+              <h1>anna palautetta</h1>
+              <Button klik={this.klikkaa('hyva')} nimi='hyvä'/>
+              <Button klik={this.klikkaa('neutraali')} nimi='neutraali'/>
+              <Button klik={this.klikkaa('huono')} nimi='huono'/>
+              <h1>statistiikka</h1>             
+              <Statistics hyva={this.state.hyva} neutraali={this.state.neutraali} huono={this.state.huono} keskiarvo={this.state.keskiarvo} kaikki={this.state.kaikki} />              
             </div>
           </div>
         )
